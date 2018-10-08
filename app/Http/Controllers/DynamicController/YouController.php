b100f6aa6de8f4ca9333f7e30c78f8401e101510
S4UR000N <robert.ascic20@gmail.com>
2018-10-08 11:43:14 +0200

    third commit

--- app/Http/Controllers/DynamicController/YouController.php
+++ app/Http/Controllers/DynamicController/YouController.php
@@ -5,56 +5,160 @@ namespace App\Http\Controllers\DynamicController;
 
 // class
 class YouController {
-	public function get() { return view('DynamicView/you'); }
-	public function post() {
+	public function get() {
 		// Final View Data
-		$View = array();
+		$BaseView = array();
 
-		// Error Boolean
-		$err_bool = 0;
 
-		// Target dir/file/extension
-		$target_dir = "uploads/";
-		$target_file = $target_dir . basename($_FILES['img_up']['name']);
-		$target_extension = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+		// Get All Image Files of the User
+		$DB = \DB::select('SELECT fid, file FROM files WHERE id = :id', ['id' => $_SESSION['id']]);
 
-		// Check if File is real/fake image
-		$check = getimagesize($_FILES['img_up']['tmp_name']);
-    if($check === false) { array_push($View, "File is not an image!"); $err_bool = 1; }
+		// Image type Holders
+		$hold = array(
+ 		 'jpeg' => array(),
+ 		 'jpg' => array(),
+ 		 'png' => array(),
+ 		 'gif' => array()
+ 		 );
 
-		// Check if File already Exists
-		if($err_bool == 0) { if(file_exists($target_file) ) { array_push($View, "File already exists!"); $err_bool = 1; } }
+		// Sort Images
+		foreach($DB as $d) {
+ 			// extract extension
+ 			$ext = pathinfo($d->file, PATHINFO_EXTENSION);
 
-		// Check File Size
-		if($err_bool == 0) { if($_FILES['img_up']['size'] > 500000) { array_push($View, "File is too large!"); $err_bool = 1; } }
-
-		// Allow only jpeg/jpg/png/gif
-		if($err_bool == 0) {
-			if($target_extension != "jpeg" && $target_extension != "jpg" && $target_extension != "png" &&  $target_extension != "gif") {
-				array_push($View, "Invalid file type!"); $err_bool = 1;
-			}
+ 			// push to one of the holders
+ 			if($ext == "jpeg") { $hold['jpeg'][$d->fid] = $d->file; }
+ 			if($ext == "jpg") { $hold['jpg'][$d->fid] = $d->file; }
+			if($ext == "png") { $hold['png'][$d->fid] = $d->file; }
+			if($ext == "gif") { $hold['gif'][$d->fid] = $d->file; }
 		}
 
-		// Save Data and Return View
-		if($err_bool == 1) { return view('DynamicView/you', compact('View')); }
-		else {
-			// Store File
-			if(move_uploaded_file($_FILES['img_up']['tmp_name'], $target_file)) {
-				// Save File Path to Database
-				$file = new \App\Files;
-			  $file->id = $_SESSION['id'];
-			  $file->file = $target_file;
-			  $file->save();
+		// Push nonempty Data  to View
+		foreach($hold as $key => $val) { if(!empty($val)) { $BaseView[$key] = $val; } }
 
-				// Return View With Success Message
-				array_push($View, "File Uploaded!");
-				return view('DynamicView/you', compact('View'));
-    	}
+
+		// Return View
+		return view('DynamicView/you', compact('BaseView'));
+	}
+	public function post() {
+		// Final View Data
+		$BaseView = array();
+		$View = array();
+
+		/* Branch Upload & Delete */
+		// Delete Branch
+		if(isset($_POST['fid'])) {
+			$DB = \DB::table('files')->where("fid", $_POST['fid'])->delete();
+			$unlink = unlink($_POST['file']);
+			if($DB == 1 && $unlink == true) {
+				return view('DynamicView/you')->with('alert', 'File Deleted Succuessfully (:');
+			}
 			else {
-			 // Return View With Fail Message
-			 array_push($View, "File Upload Failed!");
-			 return view('DynamicView/you', compact('View'));
-		  }
-	 	}
+				return view('DynamicView/you')->with('alert', '\"File Deletion Failed ):\"');
+			}
+		}
+		// Upload Branch
+		else {
+			// Get All Image Files of the User
+			$DB = \DB::select('SELECT fid, file FROM files WHERE id = :id', ['id' => $_SESSION['id']]);
+
+			// Image type Holders
+			$hold = array(
+	 		 'jpeg' => array(),
+	 		 'jpg' => array(),
+	 		 'png' => array(),
+	 		 'gif' => array()
+	 		 );
+
+			// Sort Images
+			foreach($DB as $d) {
+	 			// extract extension
+	 			$ext = pathinfo($d->file, PATHINFO_EXTENSION);
+
+	 			// push to one of the holders
+	 			if($ext == "jpeg") { $hold['jpeg'][$d->fid] = $d->file; }
+	 			if($ext == "jpg") { $hold['jpg'][$d->fid] = $d->file; }
+				if($ext == "png") { $hold['png'][$d->fid] = $d->file; }
+				if($ext == "gif") { $hold['gif'][$d->fid] = $d->file; }
+			}
+
+			// Push nonempty Data  to View
+			foreach($hold as $key => $val) { if(!empty($val)) { $BaseView[$key] = $val; } }
+
+
+			// Error Boolean
+			$err_bool = 0;
+
+			// Target dir/file/extension
+			$target_dir = "uploads/" . $_SESSION['id'];
+			$target_file = $target_dir . basename($_FILES['img_up']['name']);
+			$target_extension = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+			// Check if File is real/fake image
+			$check = getimagesize($_FILES['img_up']['tmp_name']);
+	    if($check === false) { array_push($View, "File is not an image!"); $err_bool = 1; }
+
+			// Check if File already Exists
+			if($err_bool == 0) { if(file_exists($target_file) ) { array_push($View, "File already exists!"); $err_bool = 1; } }
+
+			// Check File Size
+			if($err_bool == 0) { if($_FILES['img_up']['size'] > 800000) { array_push($View, "File is too large!"); $err_bool = 1; } }
+
+			// Allow only jpeg/jpg/png/gif
+			if($err_bool == 0) {
+				if($target_extension != "jpeg" && $target_extension != "jpg" && $target_extension != "png" &&  $target_extension != "gif") {
+					array_push($View, "Invalid file type!"); $err_bool = 1;
+				}
+			}
+
+			// Save Data and Return View
+			if($err_bool == 1) { return view('DynamicView/you', compact('BaseView', 'View')); }
+			else {
+				// Store File
+				if(move_uploaded_file($_FILES['img_up']['tmp_name'], $target_file)) {
+					// Save File Path to Database
+					$file = new \App\Files;
+				  $file->id = $_SESSION['id'];
+				  $file->file = $target_file;
+				  $file->save();
+
+					// Get All Image Files of the User
+					$DB = \DB::select('SELECT fid, file FROM files WHERE id = :id', ['id' => $_SESSION['id']]);
+
+					// Image type Holders
+					$hold = array(
+			 		 'jpeg' => array(),
+			 		 'jpg' => array(),
+			 		 'png' => array(),
+			 		 'gif' => array()
+			 		 );
+
+					// Sort Images
+					foreach($DB as $d) {
+			 			// extract extension
+			 			$ext = pathinfo($d->file, PATHINFO_EXTENSION);
+
+			 			// push to one of the holders
+			 			if($ext == "jpeg") { $hold['jpeg'][$d->fid] = $d->file; }
+			 			if($ext == "jpg") { $hold['jpg'][$d->fid] = $d->file; }
+						if($ext == "png") { $hold['png'][$d->fid] = $d->file; }
+						if($ext == "gif") { $hold['gif'][$d->fid] = $d->file; }
+					}
+
+					// Push nonempty Data  to View
+					foreach($hold as $key => $val) { if(!empty($val)) { $BaseView[$key] = $val; } }
+
+
+					// Return View With Success Message
+					array_push($View, "File Uploaded!");
+					return view('DynamicView/you', compact('BaseView', 'View'));
+	    	}
+				else {
+				 // Return View With Fail Message
+				 array_push($View, "File Upload Failed!");
+				 return view('DynamicView/you', compact('BaseView', 'View'));
+			  }
+		 	}
+		}
 	}
 }
--- resources/views/DynamicView/you.blade.php
+++ resources/views/DynamicView/you.blade.php
@@ -1,11 +1,80 @@
+<!-- MODALS.START -->
+
+<!-- Show Image Modal -->
+<div class="modal fade" id="preview">
+ <div class="modal-dialog modal-lg">
+  <div class="modal-content d-flex">
+
+   <!-- Modal Header -->
+   <div class="modal-header">
+    <span class="col-sm-5"></span>
+    <h1 class="col-sm-6">Preview</h1>
+    <button class="close" data-dismiss="modal">&times;</button>
+   </div>
+
+   <!-- Modal body -->
+   <div id="preview_body" class="modal-body row justify-content-center"></div>
+
+   <!-- Modal footer -->
+   <div class="modal-footer">
+    <button id="delete_image" class="btn btn-danger">Delete Image</button>
+   </div>
+
+  </div>
+ </div>
+</div>
+
+
+<!-- Show Download Options Modal -->
+<div class="modal fade" id="download">
+ <div class="modal-dialog modal-lg">
+  <div class="modal-content d-flex">
+
+   <!-- Modal Header -->
+   <div class="modal-header">
+    <span class="col-sm-5"></span>
+    <h1 class="col-sm-6">Download</h1>
+    <button class="close" data-dismiss="modal">&times;</button>
+   </div>
+
+   <!-- Modal body -->
+   <div id="download_body" class="modal-body row justify-content-center"></div>
+
+   <!-- Modal footer -->
+   <div class="modal-footer">
+    <button id="download_image" class="btn btn-success">Download Image</button>
+   </div>
+
+  </div>
+ </div>
+</div>
+
+<!-- MODALS.END -->
+
 <hr id="hr" class="border-0" style="height: 2px; color: #eaa81b; background-color: #eaa81b;" />
 
 <div id="read_create_delete" class="container-fluid row">
- <div class="col-sm-4 bg-dark column"></div>
- <div class="col-sm-1 bg-danger column"></div>
+ <div class="col-sm-4 bg-dark column">
+   <?php // Display User's Files Here ?>
+   @if(!empty($BaseView))
+    @foreach($BaseView as $bv)
+     @foreach($bv as $id => $img)
+      <div class="row mt-1 ml-1">
+       <div class="imgs btn text-white bg-secondary preview_init" data-fid="{{$id}}" data-file="{{$img}}" data-toggle="modal" data-target="#preview">
+       {{basename(substr_replace($img, "", strpos($img, (string)$_SESSION['id']), strlen((string)$_SESSION['id'])))}}
+       </div>
+      </div>
+     @endforeach
+    @endforeach
+
+    @else
+     <div class="no_files row justify-content-center"><img src="Assets/Icons/no_files.png"/></div>
+   @endif
+ </div>
+ <div class="col-sm-1 bg-secondary column"></div>
 
  <div class="col-sm-3 bg-dark column">
-  <form action="" method="POST" enctype="multipart/form-data" class="form row justify-content-center">
+  <form action="" method="POST" enctype="multipart/form-data" class="img_up row justify-content-center">
    <label for="img_up"><img src="Assets/Icons/img_up.png" style="cursor: cell;"/></label>
    <input id="img_up" type="file" name="img_up" accept="image/jpeg, image/jpg, image/png, image/gif" style="display: none;" onchange="img_up"/>
    <?php // Display Validation Errors here ?>
@@ -15,9 +84,13 @@
    <input id="post_img_up" name="post_img_up" type="submit" class="btn btn-warning text-dark" style="display: none;"/>
   </form>
  </div>
- <div class="col-sm-1 bg-danger column"></div>
+ <div class="col-sm-1 bg-secondary column"></div>
 
- <div class="col-sm-3 bg-dark column"></div>
+ <div class="col-sm-3 bg-dark column">
+  <div class="img_down row justify-content-center">
+   <img src="Assets/Icons/img_down.png" style="cursor: grab;" data-toggle="modal" data-target="#download" onclick="download()"/>
+  </div>
+ </div>
 </div>
 
 <style>
@@ -43,21 +116,90 @@ var $header_height = $("#header").outerHeight();
 $rcd_height = $window_height - ($hr_height + $header_height)
 $('#read_create_delete').css({ "height": $rcd_height });
 
-// .form's height
+// .form's height ˛& Icon Positioning
 var fh = $rcd_height / 3;
-$(".form").css({ "margin-top": fh })
+$(".no_files").css({ "margin-top": fh });
+$(".img_down").css({ "margin-top": fh });
+$(".img_up").css({ "margin-top": fh });
 
-
+// Remove PHP feedback
+function remove_feedback() { $('.shit').html(""); }
 
 // Upload File
-$("document").ready(function(){ $("#img_up").change(img_up); });
 function img_up() {
-  var $aa = $("#img_up");
+  var $iu = $("#img_up");
   var $piu = $("#post_img_up");
 
-  if($aa[0].files.length !== 0 ) {
-    $(".shit").html($aa[0].files[0].name);
+  if($iu[0].files.length !== 0 ) {
+    $(".shit").html($iu[0].files[0].name);
     $piu.css({ "display": "block" });
   }
- }
+}
+
+/* Launch jQ */
+$("document").ready(function() {
+  $("#img_up").change(img_up);
+  setTimeout(remove_feedback, 2000);
+});
+
+
+/* Modals Script */
+// Set Preview Image
+var preview_body = document.getElementById('preview_body');
+$(".preview_init").click(function() {
+  var fid = this.getAttribute('data-fid');
+  var file = this.getAttribute('data-file');
+
+  preview_body.innerHTML = "<img src="+ this.getAttribute('data-file')+ " data-fid="+ fid+ " data-file="+ file+ "/>";
+  $("#delete_image").click(function() {
+    delete_image(fid, file);
+  });
+});
+
+// Delete Image Option
+function delete_image(fid, file) {
+  var $form = $("<form>", { action: "", method: "POST" });
+  $form.append("<input type='text' name='fid' value='"+ fid+ "'/>");
+  $form.append("<input type='text' name='file' value='"+ file+ "'/>");
+  $form.appendTo("body").submit();
+}
+
+// Set Download Window
+function download() {
+  var $imgs = $('.imgs');
+  var $download_body = $("#download_body");
+  var $download_image_btn = $("#download_image");
+
+  var download_body_upgrade = document.createElement("div");
+  download_body_upgrade.setAttribute("class", "column");
+
+  for(var i = 0; i < $imgs.length; i++) {
+    var download_component_container = document.createElement("div");
+    download_component_container.setAttribute("class", "row");
+
+    var btn = document.createElement("button");
+
+    btn.innerHTML = $imgs.eq(i).html();
+    btn.setAttribute("data-fid", $imgs.eq(i).attr("data-fid"));
+    btn.setAttribute("data-file", $imgs.eq(i).attr("data-file"));
+    btn.setAttribute("class", "btn bg-secondary text-white mt-1");
+
+    download_component_container.appendChild(btn);
+    download_body_upgrade.appendChild(download_component_container);
+  }
+  $download_body.append(download_body_upgrade);
+}
+/* End Modals Script */
+</script>
+
+<!-- Image Deletion Feedback -->
+@if(isset($alert))
+ <script>
+ var $alert = "{{$alert}}";
+ </script>
+@endif
+
+<script>
+if(typeof $alert !== 'undefined') { setTimeout(myAlert, 1000) }
+function myAlert() { alert($alert); }
 </script>
